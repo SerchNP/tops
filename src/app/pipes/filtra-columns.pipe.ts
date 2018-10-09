97356f135f232fce84132ed01cb59bc15afe1dea
NETACIS <nat.cis05@gmail.com>
2018-10-09 13:45:19 -0500

    styled expanded row (BNC) 09-10-2018

--- src/app/pipes/filtra-columns.pipe.ts
+++ src/app/pipes/filtra-columns.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class FiltraColumnsPipe implements PipeTransform {
 
-	transform(items: any[], filtro: boolean): any {
-		if (!items || !filtro) {
+	transform(items: any[], filtro?: boolean): any {
+		if (!items || filtro === undefined) {
 			return items;
 		}
 		return items.filter(item => (item.visible === undefined ? true : item.visible) === filtro);
