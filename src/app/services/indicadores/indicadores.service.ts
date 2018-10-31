5934819ea36263c3aeb124bc29e43e1d2f8816c6
Sergio Noris <serch.np@gmail.com>
2018-10-31 10:31:28 -0600

    Se agregan las funciones para obtener los datos de un registro, para insertar, editar y cancelar

--- src/app/services/indicadores/indicadores.service.ts
+++ src/app/services/indicadores/indicadores.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { URL_SGC, HeadersGET, HeadersPOST } from '../../config/config';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/map';
+import { Indicador } from '../../interfaces/indicador.interface';
 
 @Injectable()
 export class IndicadoresService {
@@ -17,4 +18,32 @@ export class IndicadoresService {
 		const headers = HeadersGET;
 		return this.http.get(url, {headers}).map(resp => resp);
 	}
+
+	getIndicadorById(idIndicador: number) {
+		const url = URL_SGC + this.RUTA + 'getIndicadorById.json?id=' + idIndicador + '&token=' + localStorage.getItem('token');
+		const headers = HeadersGET;
+		return this.http.get(url, {headers}).map(resp => resp);
+	}
+
+	insertarIndicador(indicador: any) {
+		const url = URL_SGC + this.RUTA + 'insertarIndicador.json?token=' + localStorage.getItem('token');
+		const headers = HeadersPOST;
+		const body = JSON.stringify(indicador);
+		return this.http.post(url, body, { headers }).map(resp => resp);
+	}
+
+	modificarIndicador(indicador: Indicador) {
+		const url = URL_SGC + this.RUTA + 'modificarIndicador.json?token=' + localStorage.getItem('token');
+		const headers = HeadersPOST;
+		const body = JSON.stringify(indicador);
+		return this.http.post(url, body, { headers }).map(resp => resp);
+	}
+
+	cancelarIndicador(idIndicador: number, motivo: string) {
+		const url = URL_SGC + this.RUTA + 'cancelarIndicador.json?token=' + localStorage.getItem('token');
+		const headers = HeadersPOST;
+		const body = JSON.stringify(JSON.parse('{"indicador": ' + idIndicador + ', "motivo_cancela": "' + motivo + '"}'));
+		return this.http.post(url, body, { headers }).map(resp => resp);
+	}
+
 }
