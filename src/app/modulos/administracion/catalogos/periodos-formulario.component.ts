9afe732d9f766c5602257e3bad4ab9abd4c71dc9
Sergio Noris <serch.np@gmail.com>
2018-11-27 11:29:55 -0600

    Nueva opcion de periodos

---
+++ src/app/modulos/administracion/catalogos/periodos-formulario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-periodos-formulario',
+	templateUrl: './periodos-formulario.component.html'
+})
+
+export class PeriodosFormularioComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() { }
+
+}
---
+++ src/app/modulos/administracion/catalogos/periodos.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AccesoService, CatalogosService } from '../../../services/services.index';
+import { Derechos } from '../../../interfaces/derechos.interface';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import swal from 'sweetalert2';
+
+@Component({
+	selector: 'app-periodos',
+	templateUrl: './periodos.component.html'
+})
+
+export class PeriodosComponent implements OnInit, OnDestroy {
+
+	private subscription: Subscription;
+
+	listado: any[] = [];
+	cargando = false;
+	derechos: Derechos = {consultar: false, administrar: true, insertar: true, editar: true, cancelar: false};
+	ruta_add = ['/administracion', 'submenucat', 'periodos_form'];
+	select = false;
+	allowMultiSelect = false;
+
+	columns = [
+		{ columnDef: 'periodo',		header: 'Clave del Periodo',						cell: (formula: any) => `${formula.periodo}`},
+		{ columnDef: 'anio',		header: 'Año',										cell: (formula: any) => `${formula.anio}`},
+		{ columnDef: 'f_inicial',  	header: 'Fecha Inicial',							cell: (formula: any) => `${formula.f_inicial}`},
+		{ columnDef: 'f_final',		header: 'Fecha Final',								cell: (formula: any) => `${formula.f_final}`},
+		{ columnDef: 'estatus',		header: 'Estatus',									cell: (formula: any) => `${formula.estatus}`},
+		{ columnDef: 'f_captura',	header: 'Fecha Captura',		visible: false,		cell: (formula: any) => `${formula.f_captura}`},
+		{ columnDef: 'usu_captura',	header: 'Usuario Captura',		visible: false, 	cell: (formula: any) => `${formula.usu_captura}`}
+	];
+
+	constructor(private router: Router, private _acceso: AccesoService, private _catalogos: CatalogosService) { }
+
+	ngOnInit() {
+		this.cargando = true;
+		this.subscription = this._catalogos.getCatalogoService('PER')
+			.subscribe(
+				(data: any) => {
+					this.listado = data.catalogo;
+					this._acceso.guardarStorage(data.token);
+					this.cargando = false;
+				},
+				error => {
+					swal('ERROR', error.error.message, 'error');
+					if (error.error.code === 401) {
+						this._acceso.logout();
+					}
+				});
+	}
+
+	ngOnDestroy() {
+		// unsubscribe to ensure no memory leaks
+		this.subscription.unsubscribe();
+	}
+
+	detectarAccion(datos: any): void {
+		if (datos.accion === 'E') {
+			this.editarPeriodo(datos.row);
+		}
+	}
+
+	async editarPeriodo(periodo: any) {
+		console.log(periodo);
+	}
+
+}
